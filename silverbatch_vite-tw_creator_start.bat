22eae932380c5d0bc75bffa2fcbc9247445ace25
MisterPapaye <philippeetchtebaise@hotmail.com>
2025-05-04 19:29:55 +0200

    push recette

--- silverbatch_vite-tw_creator_start.bat
+++ silverbatch_vite-tw_creator_start.bat
@@ -1,10 +1,63 @@
 @echo off
-title Vite + Tailwind CSS Creator
+title Vite + vue Creator
+
+:title
+echo.
+echo "     ____ ___ _ __     _______ ____    ____    _  _____ ____ _   _     "
+echo "    / ___|_ _| |\ \   / / ____|  _ \  | __ )  / \|_   _/ ___| | | |    "
+echo "    \___ \| || | \ \ / /|  _| | |_) | |  _ \ / _ \ | || |   | |_| |    "
+echo "     ___) | || |__\ V / | |___|  _ <  | |_) / ___ \| || |___|  _  |    "
+echo "    |____/___|_____\_/  |_____|_| \_\ |____/_/   \_\_| \____|_| |_|    "
+echo.
+
+
+:tsorjs
+
+echo Avec quelle langage veux-tu code ?
+echo Tape 'ts' pour TypeScript ou 'js' pour JavaScript
+set /p lang="> "
+
+if /i "%lang%" neq "ts" if /i "%lang%" neq "js" (
+    cls
+    echo.
+    echo.
+    echo Choix invalide. Tape 'ts' pour TypeScript ou 'js' pour JavaScript.
+    goto :tsorjs
+) else (
+    goto :twornot
+)
+
+:twornot
+
+title Vite + vue-%lang% Creator
+
+
+echo Veux-tu code avec tailwind css (y/n) ?
+set /p iftailwind="> "
+
+if /i "%iftailwind%" neq "y" if /i "%iftailwind%" neq "n" (
+    cls
+    echo.
+    echo.
+    echo Choix invalide. Tape 'y' pour oui ou 'n' pour non.
+    goto :twornot
+) else (
+    goto :launch
+)
+
+:launch
 
 setlocal
 
-:: Met ici le chemin complet vers ton fichier PS1
-set "PS1_PATH=assets/silverbatch_vite_tw_creator.ps1"
+if /i "%iftailwind%"=="y" (
+
+    title Vite + vue-%lang% + tailwind Creator
+
+    set "PS1_PATH=assets/%lang%/vue-%lang%-tailwind.ps1"
+
+) else (
+    set "PS1_PATH=assets/%lang%/vue-%lang%.ps1"
+)
 
 :: Lancer le script PowerShell
 powershell -NoProfile -ExecutionPolicy Bypass -File "%PS1_PATH%"
